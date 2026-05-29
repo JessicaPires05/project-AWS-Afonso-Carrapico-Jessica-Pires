19c8b7f2c11b4d43c9eaf94457b65552a8e866d9
affonso25 <afonso.carrapico10@gmail.com>
2026-05-29 13:58:48 +0100

    Add initial AWS infrastructure

--- main.tf
+++ main.tf
@@ -14,4 +14,36 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name        = "project-aws-dev-vpc"
+    Project     = "project-aws"
+    Environment = "dev"
+    Owner       = "afonso-jessica"
+    ManagedBy   = "terraform"
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name        = "project-aws-dev-ec2"
+    Project     = "project-aws"
+    Environment = "dev"
+    Owner       = "afonso-jessica"
+    ManagedBy   = "terraform"
+  }
 }
